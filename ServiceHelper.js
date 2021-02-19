9f479b1f1ce8193c353c8a8fbfeb69564e87d6fc
rajwanialiya <rajwani.aliya@gmail.com>
2021-02-19 14:31:58 -0500

    removed unused code

--- ServiceHelper.js
+++ ServiceHelper.js
@@ -19,134 +19,133 @@ if ( !XMLHttpRequest.prototype.sendAsBinary ) {
 	};
 }
 
-function _getHost( baseUrl ) {
-	return /https?:\/\/([^\/]+).*/.exec(baseUrl)[1];
-}
+// function _getHost( baseUrl ) {
+// 	return /https?:\/\/([^\/]+).*/.exec(baseUrl)[1];
+// }
 
-function _handleResponse( err, res, deferred, returnRawResponse ) {
-	if ( err || !res ) {
-		ErrorActions.showServerErrorAlert( err );
-		const data = { Err: err, Res: res };
-		deferred.reject( data );
-	} else {
-		if ( AppStatus.isHealthy() ) {
-			ErrorActions.hideErrorAlertIfVisible();
-		}
+// function _handleResponse( err, res, deferred, returnRawResponse ) {
+// 	if ( err || !res ) {
+// 		ErrorActions.showServerErrorAlert( err );
+// 		const data = { Err: err, Res: res };
+// 		deferred.reject( data );
+// 	} else {
+// 		if ( AppStatus.isHealthy() ) {
+// 			ErrorActions.hideErrorAlertIfVisible();
+// 		}
 
-		const payload = returnRawResponse ? res.text : res.body;
-		deferred.resolve( payload ? payload : {} );
-	}
-}
+// 		const payload = returnRawResponse ? res.text : res.body;
+// 		deferred.resolve( payload ? payload : {} );
+// 	}
+// }
 
-function _handleAjaxResponse( status, responseText, deferred ) {
-	if (status === 200) {
-		if ( AppStatus.isHealthy() ) {
-			ErrorActions.hideErrorAlertIfVisible();
-		}
-		deferred.resolve(JSON.parse(responseText));
-	} else {
-		ErrorActions.showServerErrorAlert( responseText );
-		deferred.reject(responseText);
-	}
-}
+// function _handleAjaxResponse( status, responseText, deferred ) {
+// 	if (status === 200) {
+// 		if ( AppStatus.isHealthy() ) {
+// 			ErrorActions.hideErrorAlertIfVisible();
+// 		}
+// 		deferred.resolve(JSON.parse(responseText));
+// 	} else {
+// 		ErrorActions.showServerErrorAlert( responseText );
+// 		deferred.reject(responseText);
+// 	}
+// }
 
-function _initCancellationToken( cancellationToken, request ) {
-	if ( typeof cancellationToken !== 'object' ) {
-		return;
-	}
+// function _initCancellationToken( cancellationToken, request ) {
+// 	if ( typeof cancellationToken !== 'object' ) {
+// 		return;
+// 	}
 
-	cancellationToken.isCancelled = false;
-	cancellationToken.cancel = function() {
-		cancellationToken.isCancelled = true;
-		request.abort();
-	};
-}
+// 	cancellationToken.isCancelled = false;
+// 	cancellationToken.cancel = function() {
+// 		cancellationToken.isCancelled = true;
+// 		request.abort();
+// 	};
+// }
 
 export default {
+	// sendGETRequest( url, cancellationToken, returnRawResponse, options = {} ) {
+	// 	const deferred = Q.defer();
 
-	sendGETRequest( url, cancellationToken, returnRawResponse, options = {} ) {
-		const deferred = Q.defer();
+	// 	const request = Request
+	// 		.get( url )
+	// 		.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) );
 
-		const request = Request
-			.get( url )
-			.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) );
+	// 	if ( options.query ) {
+	// 		request.query( options.query );
+	// 	}
 
-		if ( options.query ) {
-			request.query( options.query );
-		}
+	// 	request.end( ( err, res ) => {
+	// 		_handleResponse( err, res, deferred, returnRawResponse );
+	// 	});
 
-		request.end( ( err, res ) => {
-			_handleResponse( err, res, deferred, returnRawResponse );
-		});
+	// 	_initCancellationToken( cancellationToken, request );
 
-		_initCancellationToken( cancellationToken, request );
+	// 	return deferred.promise;
+	// },
 
-		return deferred.promise;
-	},
+	// sendPUTRequest( url, payload, cancellationToken ) {
+	// 	const deferred = Q.defer();
 
-	sendPUTRequest( url, payload, cancellationToken ) {
-		const deferred = Q.defer();
+	// 	const request = Request
+	// 		.put( url )
+	// 		.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) )
+	// 		.send( payload )
+	// 		.end( ( err, res ) => {
+	// 			_handleResponse( err, res, deferred );
+	// 		});
 
-		const request = Request
-			.put( url )
-			.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) )
-			.send( payload )
-			.end( ( err, res ) => {
-				_handleResponse( err, res, deferred );
-			});
+	// 	_initCancellationToken( cancellationToken, request );
 
-		_initCancellationToken( cancellationToken, request );
+	// 	return deferred.promise;
+	// },
 
-		return deferred.promise;
-	},
+	// sendPOSTRequest( url, payload, cancellationToken ) {
+	// 	const deferred = Q.defer();
 
-	sendPOSTRequest( url, payload, cancellationToken ) {
-		const deferred = Q.defer();
+	// 	const request = Request
+	// 		.post( url )
+	// 		.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) )
+	// 		.send( payload )
+	// 		.end( ( err, res ) => {
+	// 			_handleResponse( err, res, deferred );
+	// 		});
 
-		const request = Request
-			.post( url )
-			.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) )
-			.send( payload )
-			.end( ( err, res ) => {
-				_handleResponse( err, res, deferred );
-			});
+	// 	_initCancellationToken( cancellationToken, request );
 
-		_initCancellationToken( cancellationToken, request );
+	// 	return deferred.promise;
+	// },
 
-		return deferred.promise;
-	},
+	// sendDELETERequest( url, cancellationToken, returnRawResponse ) {
+	// 	const deferred = Q.defer();
 
-	sendDELETERequest( url, cancellationToken, returnRawResponse ) {
-		const deferred = Q.defer();
+	// 	const request = Request
+	// 		.delete( url )
+	// 		.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) );
 
-		const request = Request
-			.delete( url )
-			.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) );
+	// 	request.end( ( err, res ) => {
+	// 		_handleResponse( err, res, deferred, returnRawResponse );
+	// 	});
 
-		request.end( ( err, res ) => {
-			_handleResponse( err, res, deferred, returnRawResponse );
-		});
+	// 	_initCancellationToken( cancellationToken, request );
 
-		_initCancellationToken( cancellationToken, request );
+	// 	return deferred.promise;
+	// },
 
-		return deferred.promise;
-	},
+	// sendMultipartPUTRequest( url, fileData, cancellationToken ) {
+	// 	const deferred = Q.defer();
 
-	sendMultipartPUTRequest( url, fileData, cancellationToken ) {
-		const deferred = Q.defer();
+	// 	const request = Request
+	// 		.put( url )
+	// 		.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) )
+	// 		.attach( 'file', fileData )
+	// 		.end( ( err, res ) => {
+	// 			_handleResponse( err, res, deferred );
+	// 		});
 
-		const request = Request
-			.put( url )
-			.use( Auth( { trustedHost: _getHost( AppContext.getValenceHost() ) } ) )
-			.attach( 'file', fileData )
-			.end( ( err, res ) => {
-				_handleResponse( err, res, deferred );
-			});
+	// 	_initCancellationToken( cancellationToken, request );
 
-		_initCancellationToken( cancellationToken, request );
-
-		return deferred.promise;
-	},
+	// 	return deferred.promise;
+	// },
 
 	sendMultipartMixedRequest( url, topic, file ) {
 		const boundary = new Date().getTime() + 'SmartC';
@@ -158,18 +157,18 @@ export default {
 		const deferred = Q.defer();
 		Jwt().then( function(token) {
 			const xhr = new XMLHttpRequest();
-			xhr.onreadystatechange = function() {
-				if (this.readyState === 4 ) {
-					_handleAjaxResponse( this.status, this.responseText, deferred );
-				}
-			};
-			xhr.upload.addEventListener( 'progress', progressEvent => {
-				Dispatcher.dispatch({
-					actionType: ActionNames.FILE_UPLOAD_PROGRESS,
-					bytesUploaded: progressEvent.loaded,
-					bytesTotal: progressEvent.lengthComputable ? progressEvent.total : null,
-				}, /* prevent global refresh: */ true );
-			});
+			// xhr.onreadystatechange = function() {
+			// 	if (this.readyState === 4 ) {
+			// 		_handleAjaxResponse( this.status, this.responseText, deferred );
+			// 	}
+			// };
+			// xhr.upload.addEventListener( 'progress', progressEvent => {
+			// 	Dispatcher.dispatch({
+			// 		actionType: ActionNames.FILE_UPLOAD_PROGRESS,
+			// 		bytesUploaded: progressEvent.loaded,
+			// 		bytesTotal: progressEvent.lengthComputable ? progressEvent.total : null,
+			// 	}, /* prevent global refresh: */ true );
+			// });
 			xhr.open('POST', url, true);
 			xhr.setRequestHeader('Content-Type', 'multipart/mixed; boundary=' + boundary);
 			xhr.setRequestHeader('Authorization', 'Bearer ' + token);
