4d5328f9fd4c211951e41fe9929cae406e858cc4
rajwanialiya <rajwani.aliya@gmail.com>
2021-03-30 12:48:21 -0400

    pass tests

--- config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    ADMIN_USR: "multipart-mixed.teacher",
-    ADMIN_PWD: "D2Lmultipart1",
-    HOST_URL: "https://d2llabs.desire2learn.com",
-    MODULE_ID: "1131369",
-    ORG_UNIT_ID: "12666",
-    D2L_ROUTE: "https://d2llabs.desire2learn.com"
-}
