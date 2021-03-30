4dabd2cc4f2d0821bbdd5b0425d617dfe412843a
Joshua Wilkinson <joshjeffreywilkinson@gmail.com>
2021-03-30 01:24:05 -0400

    final changes

--- testing/sampleUpload.js
+++ testing/sampleUpload.js
@@ -1,6 +1,6 @@
 const uploadToContent = require('../UploadToContent.js');
+const fileToBase64 = require('../FileToBase64');
 const request = require('superagent');
-const fs = require('fs').promises
 require('dotenv').config(); 
 
 const USR_NAME = process.env.USR_NAME;
@@ -10,12 +10,7 @@ const HOST_URL = process.env.HOST_URL;
 const MODULE_ID = process.env.MODULE_ID;
 const ORG_UNIT_ID = process.env.ORG_UNIT_ID;
 
-const base64Encode = async (file) => {
-  // read binary data
-  var bitmap = await fs.readFile(file);
-  // convert binary data to base64 encoded string
-  return new Buffer(bitmap).toString('base64');
-}
+
 
 const authenticateAdmin = async () => {
   try {
@@ -57,7 +52,7 @@ const authenticateAdmin = async () => {
 const runTest = async (file) => {
   const authToken = (await authenticateAdmin()).token; 
   const fileParts = file.split('.');
-  const fileData = await base64Encode(`sample_files/${file}`);
+  const fileData = await fileToBase64(`sample_files/${file}`);
   const fileName = fileParts[0];
   const fileExtension = fileParts[1];
   const res = await uploadToContent(HOST_URL, ORG_UNIT_ID, MODULE_ID, fileName, fileData, fileExtension, authToken);
